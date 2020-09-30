c372ad8919ebeae06c964e9695a9a1a04074e5e3
brunoterrasette <bruno.terra@sette.inf.br>
2020-09-30 20:08:22 -0300

    Cadastro imoveis validos

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,14 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import api from "../../Services/api";
 
-import { PageContainer } from '../../styles/DefaultStyles/DefaultStyles';
+import { PageContainer } from "../../styles/DefaultStyles/DefaultStyles";
+import { FormContainer, ImovelContainer, Title } from "./style";
+
+interface Imovel {
+  aluguel: string;
+  andar: string;
+  area: string;
+  armario: string;
+  bairro: string;
+  descricao: string;
+  id: string;
+  portaria: string;
+  quartos: string;
+  salasEstar: string;
+  salasJantar: string;
+  suites: string;
+  tipoImovel: string;
+  vagas: string;
+  valorCondominio: string;
+}
 
 const Home: React.FC = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async function loadData() {
+      console.log("rodando load");
+      const res = await api.get("/imoveis", {});
+      console.log(res.data);
+      if (res.data !== []) {
+        setData(res.data.imoveis);
+      } else {
+        alert("Houve um erro ao comunicar com o servidor");
+      }
+    })();
+
+    return () => {};
+  }, []);
+  // aluguel: "12345699"
+  // andar: "0"
+  // area: "0"
+  // armario: "0"
+  // bairro: "Teste Outro BAirro"
+  // descricao: ""
+  // id: "c554ff8ff8702b5a1d271e510c186a8dc6eda58faf9e8b45"
+  // portaria: "0"
+  // quartos: "0"
+  // salasEstar: "0"
+  // salasJantar: "0"
+  // suites: "0"
+  // tipoImovel: "Casa"
+  // vagas: null
+  // valorCondominio: "0"
+  function renderImovel(imovel: Imovel) {
+    console.log(imovel);
+    return <ImovelContainer>
+      <Title>Tipo de imovel: {imovel.tipoImovel}</Title>
+    </ImovelContainer>
+  }
 
   return (
     <PageContainer>
-      <div>aaaaa</div>
+      <FormContainer>
+        {data.length > 0 ? (
+          data.map((e) => renderImovel(e))
+        ) : (
+          <div>CARREGANDO</div>
+        )}
+      </FormContainer>
     </PageContainer>
-  )
+  );
 };
 
 export default Home;
---
+++ frontend/src/pages/Home/style.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+  display: grid;
+  margin: 20px auto;
+  border-radius: 10px;
+  padding: 10px 10px;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
+  align-items: center;
+  background-color: #e0e0e0;
+  box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
+`;
+
+export const ImovelContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  width: 200px;
+  height: 200px;
+  border-radius: 10px;
+  align-items: flex-start;
+  /* justify-content: flex-start; */
+  background-color: #e9e9e9;
+  box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
+`;
+
+export const Title = styled.div`
+  font-weight: bold;
+`;
